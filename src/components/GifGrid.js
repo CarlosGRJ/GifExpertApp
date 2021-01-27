f805a5afa44cca472ee99592e24da95c1e8e761c
CarlosGRJ <carlosgrj@outlook.com>
2021-01-27 09:36:10 -0600

    Fetch - imágenes deseadas

---
+++ src/components/GifGrid.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+export const GifGrid = ({ category }) => {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    getGifs();
+  }, []);
+
+  const getGifs = async () => {
+    const url =
+      'https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=aYjP53Zs1u6AgRtMIHS86kd2WuUWLsxe';
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map((img) => {
+      return {
+        id: img.id,
+        title: img.title,
+        url: img.images?.downsized_medium.url,
+      };
+    });
+
+    console.log('gifs ', gifs);
+  };
+
+  return (
+    <div>
+      <h3> {category} </h3>
+      <h3> {count} </h3>
+      <button onClick={() => setCount(count + 1)}></button>
+    </div>
+  );
+};
